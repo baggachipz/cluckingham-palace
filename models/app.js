46ee7996db422649d5f220592783dd536da678e8
Matt Curtis <matt.curtis@open-xchange.com>
2019-01-30 12:53:16 -0500

    Added first motor test with johnny-five

---
+++ models/app.js
@@ -0,0 +1,39 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", () => {
+  const motor = new five.Motor([17, 27]);
+
+  board.repl.inject({
+    motor: motor
+  });
+
+  motor.on("start", function() {
+    console.log("start", Date.now());
+  });
+
+  motor.on("stop", function() {
+    console.log("automated stop on timer", Date.now());
+  });
+
+  motor.on("forward", function() {
+    console.log("forward", Date.now());
+
+    // demonstrate switching to reverse after 5 seconds
+    board.wait(5000, function() {
+      motor.reverse(50);
+    });
+  });
+
+  motor.on("reverse", function() {
+    console.log("reverse", Date.now());
+
+    // demonstrate stopping after 5 seconds
+    board.wait(5000, function() {
+      motor.stop();
+    });
+  });
+
+  // set the motor going forward full speed
+  motor.forward(255);
+});
